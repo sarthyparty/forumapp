fb78deeac8506114e29097b17112e8c57c6b0649
Sarthak Agrawal <sarthavader@gmail.com>
2020-10-24 08:45:36 -0500

    made it look a little nicer

--- questions/forms.py
+++ questions/forms.py
@@ -4,16 +4,16 @@ from .constants import *
 
 
 class QuestionForm(forms.Form):
-    question = forms.CharField(label='Question', max_length=question_max, required=False, widget=forms.Textarea)
+    question = forms.CharField(label='', max_length=question_max, required=False, widget=forms.Textarea)
 
 
 class AnswerForm(forms.Form):
+    answer = forms.CharField(label='', max_length=answer_max, required=False,
+                             widget=forms.Textarea)
     first_name = forms.CharField(label='First Name', max_length=20, validators=[
         validators.RegexValidator(regex='^[a-zA-Z\s]*$', message="Only letters allowed!")])
     last_name = forms.CharField(label='Last Name', max_length=20, validators=[
         validators.RegexValidator(regex='^[a-zA-Z\s]*$', message="Only letters allowed!")])
-    answer = forms.CharField(label='Answer', max_length=answer_max, required=False,
-                             widget=forms.Textarea)
 
 
 class SearchForm(forms.Form):
